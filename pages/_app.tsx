cdbbd338417dc379c0f94e16c70865928b6f06e0
abduvalee95 <abduvalee95@gmail.com>
2024-06-05 07:05:48 +0900

    feat: Mui Customazed Integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  //@ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
+
+  //WebSocket, Redux,Mui ... Global integrationlarni mantigini shu erda amalga oshirishimiz mumkin 
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Component
+        {
+          ...pageProps /* app ozida mujassam barcha narsani qolgan pages routinga jonatmoqda */
+        }
+      />
+      
+    </ThemeProvider>
+  );
 }
