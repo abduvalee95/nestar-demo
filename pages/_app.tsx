39c37d0fcf65cd23114ba608f45302710171771c
abduvalee95 <abduvalee95@gmail.com>
2024-06-06 06:25:41 +0900

    feat: SASS integration SCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   //@ts-ignore
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,25 @@ import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
-const Home: NextPage=() =>{
+const Home: NextPage = () => {
   return (
-    <Container>
+    <Stack>
       <Stack flexDirection={"column"}>
-        <Box>Popular Agents</Box>
-        <Box>Top Agents</Box>
-        <Box>Top Properties</Box>
-        <Box>Events</Box>
+        <Stack>
+          <Stack className="container">Popular Agents</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top Agents</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top Properties</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Events</Stack>
+        </Stack>
+      </Stack>
     </Stack>
-  </Container>
-  )
-}
+  );
+};
 
 export default withLayoutMain(Home);
